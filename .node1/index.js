8bf260d3a11b64210b2fa9919a31be32b23adfd6
Sh Tanmoy <62763494+tanmoy108@users.noreply.github.com>
2023-08-26 16:08:26 +0600

    Delete .node1 directory

--- .node1/index.js
+++
@@ -1,2 +0,0 @@
-const module1fun = require("./module1.js");
-console.log(module1fun.fun1(2, 3), module1fun.fun2(3, 7));
--- .node1/module1.js
+++
@@ -1,6 +0,0 @@
-exports.fun1 = (a, b) => {
-  return a + b;
-}
-exports.fun2 = (a, b) => {
-  return a - b;
-}
